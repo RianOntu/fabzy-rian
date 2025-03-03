d84ce9f92c6f6cc3b73bba187f48f1445f8d20cf
RianOntu <rianontu13@gmail.com>
2025-03-04 00:37:24 +0600

    project initialization

---
+++ app/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <div className="flex">
+        <div class="logo-container">
+          <img src="/logo.webp" alt="" />
+          <div class="clip-shape"></div>
+        </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
