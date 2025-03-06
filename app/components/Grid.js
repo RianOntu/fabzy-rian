176d786cbd4c1581ca2048bdda55e6c6eb6eefa2
Rian Ontu <rianontu13@gmail.com>
2025-03-06 13:30:22 +0600

    feat:added hover effect on grid section

--- app/components/Grid.js
+++ app/components/Grid.js
@@ -1,24 +1,37 @@
 import React from "react";
 
+
 export default function Grid() {
   return (
-    <div className="w-[80%]  mx-auto mt-[30px] mb-[20px]">
+    <div className="w-[80%] mx-auto mt-[30px] mb-[20px]">
       <div className="grid grid-cols-3 grid-rows-3 gap-3">
-        <div className="col-span-1 row-span-2">
-          <img src="/bag.jpg" alt="Bag" className="w-full h-full object-cover" />
-        </div>
-        <div className="col-span-1 row-span-3">
-          <img src="/women.jpg" alt="Women" className="w-full h-full  object-cover" />
-        </div>
-        <div className="col-span-1 row-span-2">
-          <img src="/shoes-grid.jpg" alt="Shoes" className="w-full h-full  object-cover" />
-        </div>
-        <div className="col-span-1 row-span-1">
-          <img src="/glass-grid.jpg" alt="Glasses" className="w-full  object-contain" />
-        </div>
-        <div className="col-span-1 row-span-1">
-          <img src="/watch.jpg" alt="Watch" className="w-full  object-contain" />
-        </div>
+        {[
+          { src: "/bag.jpg", alt: "Bag", span: "col-span-1 row-span-2" },
+          { src: "/women.jpg", alt: "Women", span: "col-span-1 row-span-3" },
+          {
+            src: "/shoes-grid.jpg",
+            alt: "Shoes",
+            span: "col-span-1 row-span-2",
+          },
+          {
+            src: "/glass-grid.jpg",
+            alt: "Glasses",
+            span: "col-span-1 row-span-1",
+          },
+          { src: "/watch.jpg", alt: "Watch", span: "col-span-1 row-span-1" },
+        ].map((item, index) => (
+          <div
+            key={index}
+            className={`${item.span} img_hover relative overflow-hidden`}
+          >
+            <img
+              src={item.src}
+              alt={item.alt}
+              className="w-full h-full object-cover"
+            />
+            <div className="overlay absolute inset-0 bg-[#BAA2AB]"></div>
+          </div>
+        ))}
       </div>
     </div>
   );
